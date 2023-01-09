afce1a9d093bf43223c00a121aefa55d4846a74f
Eric Woolsey <ewoolsey@ualberta.ca>
2023-01-09 11:46:50 -0800

    removed unused errors

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{ConversionOverflowError, Decimal256RangeExceeded, StdError};
+use cosmwasm_std::{Decimal256RangeExceeded, StdError};
 use thiserror::Error;
 
 pub type CommonResult<T> = core::result::Result<T, CommonError>;
@@ -6,26 +6,11 @@ pub type CommonResult<T> = core::result::Result<T, CommonError>;
 #[derive(Error, Debug, PartialEq)]
 pub enum CommonError {
     #[error("{0}")]
-    Error(String),
-
-    #[error("{0}")]
     Generic(String),
 
     #[error("{0}")]
     Std(#[from] StdError),
 
     #[error("{0}")]
-    ConversionOverflowError(#[from] ConversionOverflowError),
-
-    #[error("{0}")]
     Decimal256RangeExceeded(#[from] Decimal256RangeExceeded),
-
-    #[error("Key not found: {0}")]
-    KeyNotFound(String),
-
-    #[error("Only one tx is allowed per block")]
-    MultipleTx {},
-
-    #[error("Missing Cw20HookMg")]
-    MissingHookMsg {},
 }
